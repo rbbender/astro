058f87e85d209a5b62e3a19f54b1ea871372c573
Vasilii Gratcilevskii <vgratsil@gmail.com>
2020-04-28 08:08:20 -0700

    Added asset loading

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,37 @@
 import * as _ from 'lodash';
 import * as THREE from 'three';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+//@ts-ignore
+import Sun from './assets/Sun.glb';
+
+var scene: THREE.Scene;
+var camera;
+var renderer;
 
 const createSimpleScene = () => {
-	const scene = new THREE.Scene();
-	const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	camera.position.z = 1000;
+	camera.updateProjectionMatrix();
 
-	const renderer = new THREE.WebGLRenderer();
+	const light = new THREE.DirectionalLight(0xffffff, 5);
+	light.position.z = 10;
+
+	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.physicallyCorrectLights = true;
+	renderer.gammaFactor = 2.2;
+	scene.add(light);
 	document.body.appendChild(renderer.domElement);
 }
 
+const loadResources = () => {
+	const loader = new GLTFLoader();
+	loader.load(Sun, gltf => {
+		console.log('Loaded Sun model');
+		scene.add(gltf.scene);
+	}, undefined, err => console.log(err));
+}
+
 createSimpleScene();
+loadResources();
--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,18 @@ module.exports = {
                 test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.glb$/,
+                use: 'file-loader'
             }
         ]
     },
+    devServer: {
+        contentBase: './dist'
+    },
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
         new HtmlWebpackPlugin({
             title: 'Solar system'
         })
